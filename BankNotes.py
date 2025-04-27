f25e995d569eeeebcedb85d2fdfdf640e09bee95
Tariq Mehmood <152013123+Tariq3654467@users.noreply.github.com>
2025-04-27 14:03:34 +0500

    Add files via upload

---
+++ BankNotes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 27 12:39:01 2025
+
+@author: global
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:51:19 2020
+
+@author: win10
+"""
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    variance: float 
+    skewness: float 
+    curtosis: float 
+    entropy: float
